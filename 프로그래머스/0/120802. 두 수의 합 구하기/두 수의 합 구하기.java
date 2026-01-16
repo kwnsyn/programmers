1362f497b70ad136e3b93d43d7c6ec34a6f7dadf
kwnsyn <75299356+kwnsyn@users.noreply.github.com>
2026-01-16 16:51:18 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.02 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = plusNum(num1, num2);
+        
+        return answer;
+    }
+    
+    public int plusNum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
