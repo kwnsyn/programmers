d12db224dbbc907e8f4a63cf4455d50da4fc3bce
kwnsyn <75299356+kwnsyn@users.noreply.github.com>
2026-01-24 11:03:48 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        int cup = money / 5500;
+        int balance = money % 5500;
+        
+        answer[0] = cup;
+        answer[1] = balance;
+        
+        return answer;
+    }
+}
