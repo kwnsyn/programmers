e329999a03f94ae668da056d31f569524f409bff
kwnsyn <75299356+kwnsyn@users.noreply.github.com>
2026-01-24 11:22:35 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int basicYear = 2022;
+        
+        answer = basicYear - age + 1;
+        
+        return answer;
+    }
+}
