f9f4cffae3fbf07e1105f7005c97ef8972c1f368
kwnsyn <75299356+kwnsyn@users.noreply.github.com>
2026-01-25 13:27:53 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.26 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] strArr = my_string.split("");
+        
+        for(int i=0; i<strArr.length; i++) {
+            answer += strArr[i].repeat(n);
+        }
+        
+        
+        return answer;
+    }
+}
