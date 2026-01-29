e3847eee20a96306dadb70cfe334c46862727401
kwnsyn <75299356+kwnsyn@users.noreply.github.com>
2026-01-29 16:10:29 +0900

    [level 0] Title: 순서쌍의 개수, Time: 8.34 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,17 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        List<Integer> factor = new ArrayList<Integer>();
         
         for(int i=1; i<=n; i++) {
             if(n%i == 0) {
-                factor.add(i);
+                answer++;
             }
         }
-        
-        answer = factor.size();
         
         return answer;
     }
